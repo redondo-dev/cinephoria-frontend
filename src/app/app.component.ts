361167e216f0586b30e216fa08e68da3a7cba292
redondo <riad.reda.fethi@gmail.com>
2025-10-16 21:43:53 +0200

    (#cAJgCXJ6 )feat(us3): ajout du footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
+
+
+
 export class AppComponent {
   title = 'frontend';
 }

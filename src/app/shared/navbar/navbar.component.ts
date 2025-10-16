d35dfc3c8d4e93458d726efa7e55a8c45df4702f
redondo <riad.reda.fethi@gmail.com>
2025-10-16 20:48:13 +0200

    (#vj0LI6IY)feat(US1-navbar): création du menu principal du site

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+ menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
+}

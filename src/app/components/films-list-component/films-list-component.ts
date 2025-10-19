97ab1b947053c6c2767d904062193adcff32d91f
redondo <riad.reda.fethi@gmail.com>
2025-10-19 11:59:23 +0200

    feat(us2-home): ajout des composants film-list et film-detail pour la page d'accueil

---
+++ src/app/components/films-list-component/films-list-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-films-list-component',
+  imports: [],
+  templateUrl: './films-list-component.html',
+  styleUrl: './films-list-component.scss'
+})
+export class FilmsListComponent {
+
+}

df089ca0164b28941830ddf902024dfe0f7b5d20
redondo <riad.reda.fethi@gmail.com>
2025-10-20 21:25:35 +0200

    fix(auth): correction des interceptors dans app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+<<<<<<< Updated upstream
+=======
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { HttpErrorInterceptor } from '../app/core/interceptors/http-error.interceptor';
+import { AuthInterceptor } from '../app/core/interceptors/auth.interceptor';
+>>>>>>> Stashed changes
 
 import { routes } from './app.routes';
 

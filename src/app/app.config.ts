ce771a56cb354050b3d483cd6d2af0e5418bca11
redondo <riad.reda.fethi@gmail.com>
2025-10-20 21:30:57 +0200

    fix(auth): ajout des interceptors dans app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< Updated upstream
-=======
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { HttpErrorInterceptor } from '../app/core/interceptors/http-error.interceptor';
-import { AuthInterceptor } from '../app/core/interceptors/auth.interceptor';
->>>>>>> Stashed changes
-
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routes } from './app.routes';
 
+// Import des interceptors
+import { HttpErrorInterceptor } from '../app/core/interceptors/http-error.interceptor';
+import { AuthInterceptor } from '../app/core/interceptors/auth.interceptor';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // Fournit HttpClient avec prise en charge des interceptors DI
+    provideHttpClient(withInterceptorsFromDi()),
+
+    // Déclaration explicite des interceptors
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+  ],
 };

f2861642c7fe01b24ef3929031bc61ee2b42dc35
redondo <riad.reda.fethi@gmail.com>
2025-10-20 21:30:57 +0200

    fix(auth): ajout des interceptors dans app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { HttpErrorInterceptor } from './core/http-error.interceptor';
-import { AuthInterceptor } from './services/auth.interceptor';
+import { HttpErrorInterceptor } from './core/interceptors/http-error.interceptor';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 import { routes } from './app.routes';
 

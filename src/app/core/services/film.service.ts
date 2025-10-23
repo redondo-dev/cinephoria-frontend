5fe6170845f67cfee19a8134d8877b49b442dd81
redondo <riad.reda.fethi@gmail.com>
2025-10-23 13:27:46 +0200

    Fix: mise à jour du FilmService pour l'API

--- src/app/core/services/film.service.ts
+++ src/app/core/services/film.service.ts
@@ -1,5 +1,4 @@
-import { environmentProd } from './../../../environments/.environment';
-import { environment } from './../../../environments/.environment';
+import { environment} from './../../../environments/.environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';

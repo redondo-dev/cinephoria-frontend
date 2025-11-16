8dd95461c06625795ea6c36894b6f2fe75343a8d
redondo <riad.reda.fethi@gmail.com>
2025-11-16 13:06:05 +0100

    feat(employes): mise à jour films, séances et service employés

--- src/app/components/employes/pages/seances/seances.component.ts
+++ src/app/components/employes/pages/seances/seances.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import {
   SeancesService,
   Seance,
@@ -27,38 +28,63 @@ export class SeancesComponent implements OnInit {
   constructor(
     private seancesService: SeancesService,
     private filmsService: FilmsService,
-    private sallesService: SallesService
+    private sallesService: SallesService,
+
   ) {}
 
   ngOnInit(): void {
-    this.loadSeances();
     this.loadFilms();
     this.loadSalles();
   }
 
-  loadSeances(): void {
-    this.seancesService.getAll().subscribe({
-      next: (seances) => (this.seances = seances),
-      error: (err) => console.error('Erreur chargement séances', err),
-    });
-  }
-
   loadFilms(): void {
     this.filmsService.getAll().subscribe({
-      next: (films) => (this.films = films),
+      next: (films) => {
+        this.films = films;
+        this.checkAndLoadSeances();
+      },
       error: (err) => console.error('Erreur chargement films', err),
     });
   }
 
   loadSalles(): void {
     this.sallesService.getAll().subscribe({
-      next: (salles) => (this.salles = salles),
+      next: (salles) => {
+        this.salles = salles;
+        this.checkAndLoadSeances();
+      },
       error: (err) => console.error('Erreur chargement salles', err),
+    });
+  }
+
+  private seancesLoaded = false;
+  checkAndLoadSeances(): void {
+    if (!this.seancesLoaded &&this.films.length > 0 && this.salles.length > 0) {
+     this.seancesLoaded = true;
+      this.loadSeances();
+    }
+  }
+
+  loadSeances(): void {
+    this.seancesService.getAll().subscribe({
+      next: (seances) => {
+        this.seances = seances.map((seance) => ({
+          ...seance,
+          filmTitre:
+            this.films.find((f) => f.id === seance.filmId)?.titre ||
+            'Film inconnu',
+          salleNom:
+            this.salles.find((s) => s.id === seance.salleId)?.nom ||
+            'Salle inconnue',
+        }));
+      },
+      error: (err) => console.error('Erreur chargement séances', err),
     });
   }
 
   saveSeance(): void {
     if (this.currentSeance.id) {
+      // Mise à jour
       this.seancesService
         .update(this.currentSeance.id, this.currentSeance)
         .subscribe({
@@ -69,6 +95,7 @@ export class SeancesComponent implements OnInit {
           error: (err) => console.error('Erreur modification', err),
         });
     } else {
+      // Création
       this.seancesService.create(this.currentSeance).subscribe({
         next: () => {
           this.loadSeances();
@@ -84,15 +111,6 @@ export class SeancesComponent implements OnInit {
     this.showForm = true;
   }
 
-  deleteSeance(id: number): void {
-    if (confirm('Confirmer la suppression ?')) {
-      this.seancesService.delete(id).subscribe({
-        next: () => this.loadSeances(),
-        error: (err) => console.error('Erreur suppression', err),
-      });
-    }
-  }
-
   resetForm(): void {
     this.currentSeance = this.initSeance();
     this.showForm = false;
@@ -102,9 +120,18 @@ export class SeancesComponent implements OnInit {
     return {
       filmId: 0,
       salleId: 0,
-      date_seance: '', 
+      date_seance: '',
       dateHeureDebut: '',
       dateHeureFin: '',
     };
+  }
+
+  deleteSeance(id: number): void {
+    if (confirm('Confirmer la suppression ?')) {
+      this.seancesService.delete(id).subscribe({
+        next: () => this.loadSeances(),
+        error: (err) => console.error('Erreur suppression', err),
+      });
+    }
   }
 }

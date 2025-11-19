7230c17300f92cb533f760a98bf69800485dc2ad
redondo <riad.reda.fethi@gmail.com>
2025-11-19 10:58:08 +0100

    feat(admin): maj app.config et films.component.html

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {
-  provideHttpClient, withInterceptors 
-
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/http-error.interceptor';
-
-
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,17 +15,15 @@ export const appConfig: ApplicationConfig = {
     // Fournit HttpClient avec prise en charge des interceptors DI
     provideHttpClient(
       withInterceptors([
-        authInterceptor,    // ✅ Auth en premier (ajoute le token)
-        errorInterceptor    // ✅ Error en second (gère les erreurs)
-      ]))
+        authInterceptor, // ✅ Auth en premier (ajoute le token)
+        errorInterceptor, // ✅ Error en second (gère les erreurs)
+      ])
+    ),
 
-  ]
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+  ],
 };
-    // //
-    // provideAnimations(),
-    // provideToastr({
-    //   positionClass: 'toast-bottom-right',
-    //   preventDuplicates: true,
-    // }),
-//   ],
-// };

d4667c734b0525f6f8a868286536afc216e9db53
redondo <riad.reda.fethi@gmail.com>
2025-11-23 17:07:36 +0100

    chore: remove debug components and environment test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { environment } from '../environments/environment';
-import { EnvironmentDebugComponent } from './components/environment-debug/environment-debug.component'
 
 console.log('ENV ACTIF =', environment);
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent,EnvironmentDebugComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cinephoria-frontend';
-
-
-
 }

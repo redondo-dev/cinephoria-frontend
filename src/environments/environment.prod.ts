ad4c5f89869087f2235f9a5234c8b465514927a5
redondo <riad.reda.fethi@gmail.com>
2026-06-23 12:02:37 +0200

    chore: ignore les rapports de tests generes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   // turnstileSiteKey: '0x4AAAAAADPLwDLiEmnkUVPB',
   hcaptchaSiteKey: '2cbe7b8c-5c24-4d00-84a2-359ac600dc92',
-  apiUrl: 'https://cinephoria-backend-cja3.onrender.com',
+  apiUrl: 'https://cinephoria-backend-xayv.onrender.com',
   stripePublicKey:
     'pk_test_51QdX3dAp4thofR1C7TVFsB34uppR3GwjAbEMZVQL8NBeFukZwWZf6uopIWxIBorOsjZpEVB46rkuKfLDw8ofqmc400J1UwnlgI',
 };

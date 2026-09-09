34a6d6dabb503460ff32eb04723e7c12c9dbda15
redondo <riad.reda.fethi@gmail.com>
2026-09-09 11:40:28 +0200

    chore: config environnements dev/prod + fileReplacements angular.json + nettoyage .gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  // turnstileSiteKey: '0x4AAAAAADPLwDLiEmnkUVPB',
   hcaptchaSiteKey: '2cbe7b8c-5c24-4d00-84a2-359ac600dc92',
   apiUrl: 'https://cinephoria-backend-xayv.onrender.com',
   stripePublicKey:

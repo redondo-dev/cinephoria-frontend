df4c79464ecf8b78f9313f7e44afadea71d6e472
redondo <riad.reda.fethi@gmail.com>
2026-09-12 12:39:31 +0200

    ajout d'une condition environement frocedPRODApi

--- src/app/core/interceptors/force-url.interceptor.ts
+++ src/app/core/interceptors/force-url.interceptor.ts
@@ -4,7 +4,7 @@ import { environment } from '../../../environments/environment';
 const PROD_API = 'https://cinephoria-backend-xayv.onrender.com';
 
 export const forceUrlInterceptor: HttpInterceptorFn = (req, next) => {
-  if (!environment.production) {
+  if (!environment.forceProdApi) {
     return next(req);
   }
   let url = req.url;

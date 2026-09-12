5ee151f1980e80bf22a11d651d29f5e0473df18a
redondo <riad.reda.fethi@gmail.com>
2026-09-12 12:37:13 +0200

    correctiont de l'url render dans PRO_API

--- src/app/core/interceptors/force-url.interceptor.ts
+++ src/app/core/interceptors/force-url.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
-const PROD_API = 'https://cinephoria-backend-cja3.onrender.com';
+const PROD_API = 'https://cinephoria-backend-xayv.onrender.com';
 
 export const forceUrlInterceptor: HttpInterceptorFn = (req, next) => {
   if (!environment.production) {
